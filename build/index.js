ec9f89659e2d205f321b2af4e48b3654daa38e78
Gaurav <gaurav1223344@gmail.com>
2023-12-16 20:30:07 +0530

    final changes

--- build/index.js
+++ build/index.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
 const CsvFileReader_1 = require("./CsvFileReader");
-const ConsoleReport_1 = require("./reportTargets/ConsoleReport");
 const WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
 const Summary_1 = require("./Summary");
+const HtmlReport_1 = require("./reportTargets/HtmlReport");
 // create am object that statisfies 'DataReader' interface
 const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 // Create an instance of MatchReader and pass in something satisfying
@@ -12,5 +12,5 @@ const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 const matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
 // now we can reference anyhtime matchreader.matches
-const summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
+const summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new HtmlReport_1.HtmlReport());
 summary.buildAndPrintReport(matchReader.matches);
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { CsvFileReader } from "./CsvFileReader";
 import { ConsoleReport } from "./reportTargets/ConsoleReport";
 import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { Summary } from "./Summary";
-
+import { HtmlReport } from "./reportTargets/HtmlReport";
 
 // create am object that statisfies 'DataReader' interface
 const csvFileReader = new CsvFileReader('football.csv');
@@ -17,7 +17,8 @@ matchReader.load();
 
 const summary = new Summary(
     new WinsAnalysis('Man United'),
-    new ConsoleReport()
+    new HtmlReport()
 )
 
 summary.buildAndPrintReport(matchReader.matches);
+
